12c2b47be661cc88e19c47142b19830d4051bd38
petromoldovan <petro.moldovan@simplesurance.de>
2017-05-14 19:48:47 +0200

    f

--- app/router/Router.js
+++ app/router/Router.js
@@ -6,6 +6,24 @@ import { renderToString } from 'react-dom/server'
 import App from '../index';
 import configureStore from '../store/configureStore';
 
+import Landing from '../components/Landing';
+import Contact from '../components/Contact';
+
+import { matchPath } from 'react-router-dom'
+
+
+//config object with all possible routes
+const routes = [
+    {
+        path: '/',
+        component: Landing,
+    },
+    {
+        path: '/contact',
+        component: Contact,
+    }
+]
+
 
 router.get("*", function(req, res, next) {
     const initialState = {};
@@ -27,6 +45,21 @@ router.get("*", function(req, res, next) {
 
         res.status(200).send(document);
     })
+
+    /*const promises = []
+
+    routes.some(route => {
+        // use `matchPath` here
+        const match = matchPath(req.url, route)
+        if (match)
+            promises.push(route.loadData(match))
+        return match
+    })
+
+    Promise.all(promises).then(data => {
+        // do something w/ the data so the client
+        // can access it then render the app
+    })*/
 })
 
 module.exports = router;
